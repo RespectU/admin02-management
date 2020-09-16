23760bb8140f834548036d5fa010bc457f46e222
joonseok <jsson1984@gmail.com>
2020-09-16 20:11:19 +0900

    configmap 3 update

--- src/main/java/CallBus/external/DriverService.java
+++ src/main/java/CallBus/external/DriverService.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 // 환경파일로 변경 , 추후  Config Map 해줘도 무방 단, application.yml 제거 필요
-@FeignClient(name="admin02-driver", url= "${api.url.driver}")
+@FeignClient(name="Driver", url= "${api.url.driver}")
 //@FeignClient(name="Driver", url="http://localhost:")
 /* 20200910 Cloud 올릴 때 Driver로 바꿔야함 */
 //@FeignClient(name="Driver", url="http://Driver:8080")
